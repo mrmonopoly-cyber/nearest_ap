a7fc699fbe8b3b1132de9877962652d5ea01ecc8
Alberto Damo <alberto.damo@gmail.com>
2025-12-02 12:20:16 +0100

    fix(Linux:main.cpp): updating main after the lib changes

--- target/linux/src/main.cpp
+++ target/linux/src/main.cpp
@@ -3,12 +3,14 @@
 int main(void)
 {
   using namespace nearest_ap;
+  using Node_t = nearest_ap::Node<std::size_t>;
 
-  Node<>::SendMex send_f = [](BaseTask<>::BusMex&){return BaseTask<>::BusStatus::Inactive;};
-  Node<>::RecvMex recv_f = [](){return BaseTask<>::BusMex{};};
-  Node<>::TaskSpawn spawn_f = [](BaseTask<>&){return Tasks<>::SpawnTaskReturn::Error;};
 
-  Node Drone1{send_f, recv_f, spawn_f};
+  Node_t::SendMex_t send_f = [](Node_t::Tasks_t::BusMex_t&){return BusStatus_t::Inactive;};
+  Node_t::RecvMex_t recv_f = [](){return Node_t::Tasks_t::BusMex_t{};};
+  Node_t::TaskSpawn_t spawn_f = [](Node_t::Tasks_t::BaseTask_t&){return SpawnTaskReturn::Error;};
+
+  Node_t Drone1{send_f, recv_f, spawn_f};
 
   return 0;
 }
