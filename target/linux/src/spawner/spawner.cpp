cd0eae5a28d55ca174fb5cfec71a4182a16f06b7
Alberto Damo <alberto.damo@gmail.com>
2025-12-13 10:26:55 +0100

    feat(linux spawner): added possibility to stop the running tasks, also the destroyer of the spawner will clean the running thread

--- target/linux/src/spawner/spawner.cpp
+++ target/linux/src/spawner/spawner.cpp
@@ -1,6 +1,4 @@
 #include "spawner.h"
-#include <functional>
-#include <chrono>
 #include <thread>
 #include <iostream>
 
@@ -9,19 +7,21 @@ using namespace nearest_ap;
 using namespace std::chrono;
 using namespace std::this_thread;
 
-static void _start_task(std::function<void(void)> f)
+void SpawnerLinux_t::_run_task(BaseTask_t*const t, const Millis_t freq) noexcept
 {
-  std::thread th{f};
-  th.detach();
+  const TaskId t_index = t->id();
+  if (t_index < static_cast<TaskId>(InteractibleTask::TASK_COUNT))
+  {
+    m_tasks[t_index] = TaskWrapper{t, freq};
+    TaskWrapper& ref_task = m_tasks[t_index]; 
+    std::thread th{s_runner_f, &ref_task};
+    th.detach();
+  }
 }
 
 void SpawnerLinux_t::start_task(BaseTask_t* t) noexcept 
 {
-  std::cout
-    << "staring task: " << t->id()
-    << std::endl;
-
-  _start_task([t]{while(true)t->run();});
+  return start_task(t,0);
 }
 
 void SpawnerLinux_t::start_task(BaseTask_t* t, Millis_t f) noexcept
@@ -31,13 +31,57 @@ void SpawnerLinux_t::start_task(BaseTask_t* t, Millis_t f) noexcept
     << " with freq: " << f
     << std::endl;
 
-  _start_task([t,f]
-        {
-          while(true)
-          {
-            t->run();
-            sleep_for(milliseconds(f));
-          }
-        }
-      );
+  _run_task(t,f);
+}
+
+SpawnerLinux_t::~SpawnerLinux_t()
+{
+  for(auto& task: m_tasks)
+  {
+    std::cout
+      << "stopping task: "
+      << task.id()
+      << std::endl;
+    task.stop();
+  }
+}
+
+SpawnerLinux_t::TaskWrapper& SpawnerLinux_t::TaskWrapper::operator=(TaskWrapper&&other)
+{
+  if (this != &other)
+  {
+    this->m_base_task = other.m_base_task;
+    this->m_run = other.m_run;
+    this->m_freq = other.m_freq;
+
+    other.m_base_task = nullptr;
+    other.m_run = false;
+    other.m_freq =0;
+  }
+  return *this;
+}
+
+bool SpawnerLinux_t::TaskWrapper::can_execute() const noexcept
+{
+  return m_run;
+}
+
+TaskId SpawnerLinux_t::TaskWrapper::id() const noexcept
+{
+  if (m_base_task)
+  {
+    return m_base_task->id();
+  }
+  return 99;
+}
+
+void SpawnerLinux_t::TaskWrapper::stop() noexcept
+{
+  m_run = false;
+}
+
+void SpawnerLinux_t::TaskWrapper::run() noexcept
+{
+  m_base_task->run();
+  std::this_thread::sleep_for(std::chrono::milliseconds(m_freq));
 }
--- target/linux/src/spawner/spawner.h
+++ target/linux/src/spawner/spawner.h
@@ -1,11 +1,43 @@
 #pragma once
 
+#include <array>
 #include <nearest_ap/nearest_ap.hpp>
 
 namespace nearest_ap
 {
 class SpawnerLinux_t : public Spawner_t
   {
+    private:
+      class TaskWrapper 
+      {
+        public:
+          TaskWrapper(
+              const bool m_run=false,
+              const Millis_t freq=0,
+              BaseTask_t *const m_base_task=nullptr) noexcept
+              : m_run(m_run), m_freq(freq), m_base_task(m_base_task) {}
+
+          TaskWrapper(BaseTask_t *const m_base_task) noexcept
+              : m_run(true), m_freq(0), m_base_task(m_base_task) {}
+
+          TaskWrapper(BaseTask_t *const m_base_task, const Millis_t freq) noexcept
+              : m_run(true), m_freq(freq), m_base_task(m_base_task) {}
+
+          TaskWrapper(TaskWrapper&&) =default;
+          TaskWrapper& operator=(TaskWrapper&&);
+
+          bool can_execute() const noexcept;
+          TaskId id() const noexcept;
+
+          void stop() noexcept;
+          void run() noexcept;
+
+        private:
+          bool m_run;
+          Millis_t m_freq;
+          BaseTask_t* m_base_task;
+      };
+
     public:
       explicit SpawnerLinux_t(){}
 
@@ -13,9 +45,21 @@ class SpawnerLinux_t : public Spawner_t
       SpawnerLinux_t operator=(const SpawnerLinux_t& ) =delete;
 
       SpawnerLinux_t(SpawnerLinux_t&& ) = default;
-      SpawnerLinux_t& operator=(SpawnerLinux_t&& ) = default;
+      SpawnerLinux_t& operator=(SpawnerLinux_t&& ) = delete;
 
       void start_task(BaseTask_t* t) noexcept override;
       void start_task(BaseTask_t* t, Millis_t f) noexcept override;
+
+      ~SpawnerLinux_t();
+
+    private:
+      void _run_task(BaseTask_t*const t, const Millis_t freq) noexcept;
+
+    private:
+      static constexpr TaskId s_num_tasks = static_cast<TaskId>(InteractibleTask::TASK_COUNT);
+      static constexpr auto s_runner_f = 
+        [](TaskWrapper* r_task){while(r_task->can_execute())r_task->run();};
+
+      std::array<TaskWrapper, s_num_tasks> m_tasks;
   };
 }
